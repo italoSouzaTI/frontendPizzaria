7103eafc0cca1b2900d04c41b34743eae0526e70
Italo Araujo <italoasouzat.i@gmail.com>
2022-05-27 21:55:17 -0300

    Criando header

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,18 @@
 import { canSSRAuth } from '../../utils/canSSRAuth'
+import Head from 'next/head'
 
+import { Header } from '../../components/header'
 export default function Dashboard () {
     return (
-        <div>
-            <h1>bem vindo ao painel</h1>
-        </div>
+        <>
+            <Head>
+                <title>Painel -  Sujeito Pizaria</title>
+            </Head>
+            <Header />
+            <div>
+                <h1>bem vindo ao painel</h1>
+            </div>
+        </>
     )
 }
 export const getServerSideProps = canSSRAuth(async (ctx) => {
